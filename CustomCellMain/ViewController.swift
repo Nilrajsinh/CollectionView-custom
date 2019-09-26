ba4cd8259430750589b42f1fdd505bb3fef1a9a2
nil <Nilrajsinh6005@ICLOUD.COM>
2019-09-26 22:45:39 +0530

    Initial Commit

---
+++ CustomCellMain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCellMain
+//
+//  Created by Nilrajsinh Vaghela on 26/09/19.
+//  Copyright © 2019 Cyphers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
